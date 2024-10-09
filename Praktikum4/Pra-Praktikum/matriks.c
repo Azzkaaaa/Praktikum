75c06634d3b9383a39beec6589a38aa83b4ea6d2
Azzkaaaa <19623121@std.stei.itb.ac.id>
2024-10-09 15:41:07 +0700

    feat: matriks.c

--- Praktikum4/Pra-Praktikum/matriks.c
+++ Praktikum4/Pra-Praktikum/matriks.c
@@ -37,6 +37,91 @@ void copyMatrix(Matrix mIn, Matrix *mOut){
 }
 
 void readMatrix(Matrix *m, int nRow, int nCol){
+    int i;
+    int j;
     isMatrixIdxValid(nRow, nCol);
     createMatrix(m, nRow, nCol);
+    for (i = 0; i < ROW_EFF(*m); i++){
+        for (j = 0; j < COL_EFF(*m); j++){
+            scanf("%d", &ELMT(*m, i, j));
+        }
+    }
+}
+
+void displayMatrix(Matrix m){
+    int i;
+    int j;
+    for (i = 0; i < ROW_EFF(m); i++){
+        for (j = 0; j < COL_EFF(m); j++){
+            printf("%d", ELMT(m, i, j));
+            if (j != getLastIdxCol(m) - 1){
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+Matrix addMatrix(Matrix m1, Matrix m2){
+    int i;
+    int j;
+    Matrix result;
+    createMatrix(&result, ROW_EFF(m1), COL_EFF(m1));
+    for (i = 0; i < ROW_EFF(m1); i++){
+        for (j = 0; j < COL_EFF(m1); j++){
+            ELMT(result, i, j) = ELMT(m1, i, j) + ELMT(m2, i, j);
+        }
+    }
+    return result;
+}
+
+Matrix subtractMatrix(Matrix m1, Matrix m2){
+    int i;
+    int j;
+    Matrix result;
+    createMatrix(&result, ROW_EFF(m1), COL_EFF(m1));
+    for (i = 0; i < ROW_EFF(m1); i++){
+        for (j = 0; j < COL_EFF(m1); j++){
+            ELMT(result, i, j) = ELMT(m1, i, j) - ELMT(m2, i, j);
+        }
+    }
+    return result;
+}
+
+Matrix multiplyMatrix(Matrix m1, Matrix m2){
+    int i;
+    int j;
+    Matrix result;
+    createMatrix(&result, ROW_EFF(m1), COL_EFF(m1));
+    for (i = 0; i < ROW_EFF(m1); i++){
+        for (j = 0; j < COL_EFF(m1); j++){
+            ELMT(result, i, j) = ELMT(m1, i, j) * ELMT(m2, i, j);
+        }
+    }
+    return result;
+}
+
+Matrix multiplyByConst(Matrix m, ElType x){
+    int i;
+    int j;
+    for (i = 0; i < ROW_EFF(m); i++){
+        for (j = 0; j < COL_EFF(m); j++){
+            ELMT(m, i, j) = ELMT(m, i, j) * x;
+        }
+    }
+    return m;
+}
+
+void pMultiplyByConst(Matrix *m, ElType k){
+    int i;
+    int j;
+    for (i = 0; i < ROW_EFF(*m); i++){
+        for (j = 0; j < COL_EFF(*m); j++){
+            ELMT(*m, i, j) = ELMT(*m, i, j) * k;
+        }
+    }
+}
+
+boolean isMatrixEqual(Matrix m1, Matrix m2){
+    
 }
